26587173a56644db7555e7d43b94ea1fbdf51e86
poe1993 <poe1993@yahoo.com>
2020-08-10 16:17:53 -0400

    Finished homework

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace OurDotNetApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# My name is Eric. I'm a student at Suncoast Developers' Guild");
+        }
     }
-  }
 }
